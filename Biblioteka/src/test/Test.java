2571ee2f8ca1cc5d3a849c4ffd1e5f76825c48b7
djordjije11 <djordjijeradovic24@gmail.com>
2023-02-28 11:33:32 +0100

    Uvedeni toString, hashCode i equals za Autora;

---
+++ Biblioteka/src/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+import biblioteka.Autor;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Autor a = new Autor();
+		a.setIme("Pera");
+		a.setIme("Peric");
+		Autor b = new Autor();
+		b.setIme("Pera");
+		b.setIme("Peric");
+		System.out.println(a.hashCode() == b.hashCode());
+	}
+
+}
