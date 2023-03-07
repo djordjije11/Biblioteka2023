466d0b5d9134578af7725ed83d7c04f81832c249
djordjije11 <djordjijeradovic24@gmail.com>
2023-03-07 09:53:10 +0100

    Revert "dodat komentar u klasu Biblioteka;"
    This reverts commit b1b251a5c7c9374aa406c457ebc54819d98eb467.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -10,9 +10,6 @@ import biblioteka.interfejs.BibliotekaInterfejs;
 public class Biblioteka implements BibliotekaInterfejs {
 	private List<Knjiga> knjige = new ArrayList();
 	
-	
-	
-	//Pravi komentar, pravi kod
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		if(knjiga == null)
