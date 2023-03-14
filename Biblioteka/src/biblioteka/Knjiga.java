1fa77fc6a80119331da2988549f3d758c2048b92
djordjije11 <djordjijeradovic24@gmail.com>
2023-03-14 09:10:59 +0100

    added validation for class Knjiga;

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -14,12 +14,21 @@ public class Knjiga {
 		return naslov;
 	}
 	public void setNaslov(String naslov) {
+		if(naslov == null) {
+			throw new NullPointerException("Naslov ne sme biti null");
+		}
+		if(naslov.isEmpty()) {
+			throw new IllegalArgumentException("Naslov ne sme biti prazno");
+		}
 		this.naslov = naslov;
 	}
 	public long getIsbn() {
 		return isbn;
 	}
 	public void setIsbn(long isbn) {
+		if(isbn <= 0) {
+			throw new IllegalArgumentException("ISBN ne sme biti nula ili manji od nule");
+		}
 		this.isbn = isbn;
 	}
 	public List<Autor> getAutori() {
@@ -32,12 +41,21 @@ public class Knjiga {
 		return izdavac;
 	}
 	public void setIzdavac(String izdavac) {
+		if(izdavac == null) {
+			throw new NullPointerException("Izdavac ne sme biti null");
+		}
+		if(izdavac.isEmpty()) {
+			throw new IllegalArgumentException("Izdavac ne sme biti prazno");
+		}
 		this.izdavac = izdavac;
 	}
 	public int getIzdanje() {
 		return izdanje;
 	}
 	public void setIzdanje(int izdanje) {
+		if(izdanje < 1) {
+			throw new IllegalArgumentException("Izdanje ne sme biti broj manji od 1.");
+		}
 		this.izdanje = izdanje;
 	}
 }
