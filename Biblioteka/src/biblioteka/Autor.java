7b19318d485e69df1b5e6ca3d81232b51d5e5d52
djordjije11 <djordjijeradovic24@gmail.com>
2023-03-21 09:07:51 +0100

    dodat javadoc za klasu Autor

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -2,9 +2,24 @@ package biblioteka;
 
 import java.util.Objects;
 
+/**
+ * Predstavlja autora knjige.
+ * 
+ * Autor ima samo ime i prezime, nema identifikator. 
+ *  
+ * @author djordjije11
+ * @since 0.1.0
+ *
+ */
 public class Autor {
 
+	/**
+	 * Author's firstname.
+	 */
 	private String ime;
+	/**
+	 * Author's lastname.
+	 */
 	private String prezime;
 	
 	public Autor() {};
@@ -13,9 +28,24 @@ public class Autor {
 		this.prezime = prezime;
 	}
 	
+	/**
+	 * Returns author's firstname.
+	 * 
+	 * @return author's firstname as String.
+	 */
 	public String getIme() {
 		return ime;
 	}
+	/**
+	 * Postavlja vrednost atributa ime.
+	 * 
+	 * Ime ne sme biti null niti prazan String.
+	 * 
+	 * @param ime
+	 * 
+	 * @throws NullPointerException ako se unese null vrednost za ime
+	 * @throws IllegalArgumentException ako se unese prazan String za ime
+	 */
 	public void setIme(String ime) {
 		if(ime == null) {
 			throw new NullPointerException("Ime ne sme biti null");
@@ -25,9 +55,24 @@ public class Autor {
 		}
 		this.ime = ime;
 	}
+	/**
+	 * Returns author's lastname.
+	 * 
+	 * @return author's lastname as String.
+	 */
 	public String getPrezime() {
 		return prezime;
 	}
+	/**
+	 * Postavlja vrednost atributa prezime.
+	 * 
+	 * Prezime ne sme biti null niti prazan String.
+	 * 
+	 * @param prezime
+	 * 
+	 * @throws NullPointerException ako se unese null vrednost za prezime
+	 * @throws IllegalArgumentException ako se unese prazan String za prezime
+	 */
 	public void setPrezime(String prezime) {
 		if(prezime == null) {
 			throw new NullPointerException("Prezime ne sme biti null");
@@ -41,10 +86,24 @@ public class Autor {
 	public String toString() {
 		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
 	}
+	/**
+	 * Racuna hash code na osnovu imena i prezimena autora
+	 * 
+	 * @return hash code izracunat na osnovu imena i prezimena
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(ime, prezime);
 	}
+	/**
+	 * Poredi dva autora prema imenu i prezimenu
+	 * 
+	 * @return 
+	 * <ul>
+	 * 		<li> true - ako su ime i prezime autora isti </li>
+	 * 		<li> false - u suprotnom </li>
+	 * </ul?
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
